d813e6b43ff46435036c3a2a8925d08243745537
Sasha Hordiichuk <sg014877@gmail.com>
2022-12-03 16:22:59 +0200

    add main page to GitHub

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function initMap() {
+	var opt = {
+		center: {lat: 50.61981765981194, lng: 26.2515258528243},
+		zoom: 13
+	}
+
+	var map = new google.maps.Map(document.getElementById("map"), opt);
+}
